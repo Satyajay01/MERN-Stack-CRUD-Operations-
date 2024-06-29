b52a397afce978bb6d13110eb637dd8fc11143c7
satyajay01 <satyajaydibya0@gmail.com>
2024-06-29 23:34:13 +0530

    React router created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: "Home page",
+    },
+    {
+      path:"/add",
+      element: "User add Page",
+    },
+    {
+      path:"/edit",
+      element: "Update User Page",
+    },
+  ])
   return (
-    <div>App</div>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
